d6b19193075bb30bfeda13bf0b11c0331f65a21d
Haoxi2002 <3020001195@tju.edu.cn>
2024-10-04 17:22:03 +0800

    update .gitignore

--- work.py
+++
@@ -1,9 +0,0 @@
-file = open('result_long_term_forecast.txt', 'r')
-lines = file.readlines()
-outfile = open('result_long_term_forecast.csv', 'w')
-for i, line in enumerate(lines):
-    if i % 3 == 0:
-        outfile.write(line.split('_')[1]+',')
-    if i % 3 == 1:
-        l = line.split(', ')
-        outfile.write(l[-3].split(':')[-1]+','+l[-2].split(':')[-1]+','+l[-1].split(':')[-1])
