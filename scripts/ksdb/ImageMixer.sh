c14c81a546f496ba2e211207bbbbeb8cae2e2447
Haoxi2002 <3020001195@tju.edu.cn>
2024-10-06 13:57:53 +0800

    better config

--- scripts/ksdb/ImageMixer.sh
+++ scripts/ksdb/ImageMixer.sh
@@ -18,7 +18,7 @@ nohup python run.py \
       --patch_size 36 36 \
       --token_mlp_dim 256 \
       --channel_mlp_dim 32 \
-      --n_blocks 4 \
+      --n_blocks 2 \
       --dropout 0 \
       --batch_size 512 \
       --learning_rate 0.005 \
