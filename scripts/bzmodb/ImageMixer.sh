9ca464d469faac76c2e007450903f807ffc47654
Haoxi2002 <3020001195@tju.edu.cn>
2024-10-09 11:27:47 +0800

    better config

--- scripts/bzmodb/ImageMixer.sh
+++ scripts/bzmodb/ImageMixer.sh
@@ -17,7 +17,7 @@ nohup python run.py \
       --patch_size 36 36 \
       --token_mlp_dim 256 \
       --channel_mlp_dim 8 \
-      --n_blocks 2 \
+      --n_blocks 4 \
       --dropout 0 \
       --batch_size 512 \
       --learning_rate 0.001 \
