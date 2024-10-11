c04f5bec3ff32d045a33f9deb880bb8975d65711
Haoxi2002 <3020001195@tju.edu.cn>
2024-10-12 04:30:30 +0800

    update requirements

---
+++ scripts/ksdb/ImageMixer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+nohup python run.py \
+      --is_training 1 \
+      --task_id ksdb_ImageMixer \
+      --model ImageMixer \
+      --data ksdb \
+      --dir_path ./data/PPIO \
+      --data_path ksdb.csv \
+      --freq 5min \
+      --seq_len 288 \
+      --label_len 144 \
+      --pred_len 288  \
+      --h $((288 * 2)) \
+      --use_multi_gpu \
+      > ksdb_ImageMixer.txt 2>&1 &
