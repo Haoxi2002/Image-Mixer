03a975fc7107bce8d991dae6196d470e5214f2fa
Haoxi2002 <3020001195@tju.edu.cn>
2024-10-12 10:41:38 +0800

    update model

--- model/ImageMixer.py
+++ model/ImageMixer.py
@@ -24,9 +24,19 @@ class Model(nn.Module):
              in range(args.n_blocks)])
         self.head_layer_norm = nn.LayerNorm(args.hidden_dim)
         self.flatten = nn.Flatten(start_dim=-2)
-        self.static_embedding2 = nn.Linear(9, self.token_dim)
-        self.mlp = MlpBlock(self.token_dim * (args.hidden_dim + 1), self.token_dim * (args.hidden_dim + 1) * 2)
-        self.linear = nn.Linear(self.token_dim * (args.hidden_dim + 1), args.pred_len)
+        self.static_embedding2 = nn.Sequential(
+            nn.Linear(9, self.token_dim),
+            nn.GELU(),
+            nn.Dropout(args.dropout),
+            nn.Linear(self.token_dim, self.token_dim * args.hidden_dim),
+            nn.Dropout(args.dropout),
+        )
+        self.mlp = nn.Sequential(
+            MlpBlock(self.token_dim * args.hidden_dim * 2, self.token_dim * args.hidden_dim * 4),
+            nn.GELU(),
+            nn.Linear(self.token_dim * args.hidden_dim * 2, self.token_dim * args.hidden_dim)
+        )
+        self.linear = nn.Linear(self.token_dim * args.hidden_dim * 2, args.pred_len)
 
     """
     input:    
@@ -45,22 +55,22 @@ class Model(nn.Module):
         # encoder
         x = self.conv_embedding(x)
         x = einops.rearrange(x, 'b c h w -> b (h w) c')
-        x = self.embedding_feedforward(x)
-        e_static = self.static_embedding1(static)
-        e_static = torch.reshape(e_static, (bc, self.token_dim, self.args.hidden_dim))
-        x = self.fc_fusion(torch.cat((x, e_static), dim=2))
+        # x = self.embedding_feedforward(x)
+        # e_static = self.static_embedding1(static)
+        # e_static = torch.reshape(e_static, (bc, self.token_dim, self.args.hidden_dim))
+        # x = self.fc_fusion(torch.cat((x, e_static), dim=2))
 
         # backbone
         for l in self.blocks:
             x = l(x)
 
         # decoder (b, p, c)  b=batch_size p=patches c=channel
-        x = self.head_layer_norm(x)
+        # x = self.head_layer_norm(x)
         x = self.flatten(x)
         x = torch.unsqueeze(x, dim=1)
-        d_static = self.static_embedding2(static)
-        x = torch.cat([d_static, x], dim=2)
-        x = self.mlp(x)
+        # d_static = self.static_embedding2(static)
+        # x = torch.cat([d_static, x], dim=2)
+        # x = self.mlp(x)
         x = self.linear(x)
         x = torch.transpose(x, 1, 2)  # (bs, pred_len, 1)
 
