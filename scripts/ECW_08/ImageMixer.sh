7d88477e1641ea6818c3f37925b919398faaac3e
Haoxi2002 <3020001195@tju.edu.cn>
2024-10-15 21:07:15 +0800

    find better config

--- scripts/ECW_08/ImageMixer.sh
+++ scripts/ECW_08/ImageMixer.sh
@@ -2,6 +2,7 @@
 
 nohup python run.py \
       --is_training 1 \
+      --draw_test 0 \
       --task_id ECW_ImageMixer \
       --model ImageMixer \
       --data ECW \
@@ -13,12 +14,20 @@ nohup python run.py \
       --pred_len 24  \
 		  --inverse \
       --h $((48)) \
+      --lw 1 \
+      --expand 2 \
+      --channel 1 \
+      --lc 0 0 0 \
+      --bc 0 0 0 \
       --hidden_dim 16 \
       --channel_mlp_dim 32 \
       --patch_size 6 6 \
       --token_mlp_dim 256 \
       --n_blocks 2 \
+      --train_epochs 100 \
+      --batch_size 512 \
+      --patience 50 \
       --dropout 0 \
-      --learning_rate 0.0003 \
+      --learning_rate 0.001 \
       --use_multi_gpu \
       > ECW_08_ImageMixer.txt 2>&1 &
