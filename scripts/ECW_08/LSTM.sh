b67e93c8502aa6748378682694e0a5d7e1ea8342
Haoxi2002 <3020001195@tju.edu.cn>
2024-10-15 16:12:32 +0800

    add LSTM

---
+++ scripts/ECW_08/LSTM.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+nohup python run.py \
+      --is_training 1 \
+      --task_id ECW_LSTM \
+      --model LSTM \
+      --data ECW \
+      --dir_path ./data/ECW \
+      --data_path ECW_08.csv \
+      --freq h \
+      --seq_len 48 \
+      --label_len 24 \
+      --pred_len 24  \
+		  --inverse \
+		  --d_model 64 \
+		  --e_layers 2 \
+      --use_multi_gpu \
+      > ECW_08_LSTM.txt 2>&1 &
