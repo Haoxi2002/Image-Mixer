9a764d60decb5b2574c1dd1fe64314c22828fedb
Haoxi2002 <3020001195@tju.edu.cn>
2024-10-18 15:08:59 +0800

    add models scripts

---
+++ scripts/ECW_08/KAE_Informer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+nohup python run.py \
+      --is_training 1 \
+      --task_id ECW_KAE_Informer \
+      --model KAE_Informer \
+      --data ECW \
+      --dir_path ./data/ECW \
+      --data_path ECW_08.csv \
+      --freq h \
+      --seq_len 48 \
+      --label_len 24 \
+      --pred_len 24  \
+		  --inverse \
+		  --d_model 128 \
+		  --n_heads 8 \
+		  --e_layers 2 \
+		  --d_layers 1 \
+		  --d_ff 512 \
+      --dropout 0 \
+      --use_multi_gpu \
+      > ECW_08_KAE_Informer.txt 2>&1 &
